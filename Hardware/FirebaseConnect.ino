5980d0b22479dc4c9b1d8ce901d075d8bbdf0fd6
JasonWu2020 <48867921+JasonWu2020@users.noreply.github.com>
2021-02-09 17:40:36 -0800

    Update FirebaseConnect.ino
    Wheee

--- Hardware/FirebaseConnect.ino
+++ Hardware/FirebaseConnect.ino
@@ -1,47 +1,65 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-// Set these to run example.
-#define FIREBASE_HOST ""
+#define FIREBASE_HOST "espfirebaseconfig-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH ""
-#define WIFI_SSID ""
+#define WIFI_SSID "BlueLaser"
 #define WIFI_PASSWORD ""
 
 void setup() {
-  Serial.begin(9600);
-
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
+    Serial.begin(9600);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
-  pinMode(2, OUTPUT);
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    }
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Firebase.stream("");
+    if (Firebase.success()) {
+        Serial.println("Success!");
+    }
+    else {
+        Serial.println("Sad arduino sounds");
+    }
 }
-
-boolean currentOn = false;
-boolean newOn = false; 
+char ch = '0';
+boolean led1On;
+boolean led2On;
+boolean led3On;
+String led1Color;
+String led2Color;
+String led3Color;
+boolean newOn = false;
 void loop() {
-  // set bool value
-  newOn = Firebase.getBool("led");
-  // handle error
-  if (Firebase.failed()) {
+    Serial.println("Hello");
+    if (Firebase.available()) {
+        FirebaseObject a = Firebase.readEvent();
+        String path = a.getString("path");
+        JsonVariant js = a.getJsonVariant(path);
+        Serial.println(js.is<bool>());
+        js.prettyPrintTo(Serial);
+        if (js.is<int>()) {
+            Serial.println(path + " " + Firebase.getInt(path)); //ESP32 :))
+        }
+        if (js.is<bool>()) {
+            Serial.println(path + " " + Firebase.getBool(path)); //ESP32 :))
+        }
+        if (js.is<const char*>()) {
+            Serial.println(path + " " + Firebase.getString(path)); //ESP32 :))
+        }
+        if (js.is<double>()) {
+            Serial.println(path + " " + Firebase.getFloat(path)); //ESP32 :))
+        }
+    }
+    if (Firebase.failed()) {
       Serial.print("setting /truth failed:");
       Serial.println(Firebase.error());  
       return;
-  }
-  if (newOn && !currentOn) {
-    digitalWrite(2, HIGH);
-    currentOn = true;
-  }
-  else if (!newOn && currentOn) {
-    digitalWrite(2, LOW);
-    currentOn = false;
-  }
-  delay(10000);
+    }
+    delay(1000);
+    
 }
